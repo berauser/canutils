b0bb28a658296afdb7711d13639a9d0a9f0f8349
Benjamin Rauser <benjamin.rauser@gmx.net>
2017-09-26 19:17:13 +0200

    add compare operator for CANMessage

--- src/CANMessage.h
+++ src/CANMessage.h
@@ -22,12 +22,32 @@ enum struct CANFlag : uint32_t
 	Error    = 0x20000000U //!< Error
 };
 
+inline uint32_t operator| ( const CANFlag& flag, const uint32_t data )
+{
+	return data & static_cast<uint32_t>(flag);
+}
+
+inline uint32_t operator| ( const uint32_t data, const CANFlag& flag )
+{
+	return static_cast<uint32_t>(flag) & data;
+}
+
 enum struct CANMask : uint32_t
 {
 	Standard = 0x000007FFU,
 	Extended = 0x1FFFFFFFU,
 	Error    = 0x1FFFFFFFU
 };
+
+inline uint32_t operator& ( const CANMask& mask, const uint32_t data )
+{
+	return data & static_cast<uint32_t>(mask);
+}
+
+inline uint32_t operator& ( const uint32_t data, const CANMask& mask )
+{
+	return static_cast<uint32_t>(mask) & data;
+}
 
 struct CANMessage final
 {
@@ -37,6 +57,15 @@ struct CANMessage final
 	uint8_t reserved1;
 	uint8_t reserved2;
 	uint8_t data[MAX_DATA_LENGTH];
+
+	bool operator==( const CANMessage& m1 ) const
+	{
+		return ( can_id == m1.can_id && can_dlc == m1.can_dlc
+				&& data[0] == m1.data[0] && data[1] == m1.data[1]
+				&& data[2] == m1.data[2] && data[3] == m1.data[3]
+				&& data[4] == m1.data[4] && data[5] == m1.data[5]
+				&& data[6] == m1.data[6] && data[7] == m1.data[7]);
+	}
 };
 
 } /* namespace CanSocket */
