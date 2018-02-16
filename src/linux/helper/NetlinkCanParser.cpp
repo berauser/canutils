756a857d5af9f92c7df0e2d1518e0f449e640c07
Benjamin Rauser <benjamin.rauser@gmx.net>
2018-02-16 23:10:09 +0100

    Implement NetlinkCanParser

--- src/linux/helper/NetlinkCanParser.cpp
+++ src/linux/helper/NetlinkCanParser.cpp
@@ -3,6 +3,8 @@
 #include <cstdio>
 #include <cstring>
 
+#include <linux/can/netlink.h>
+
 namespace CanSocket
 {
   
@@ -16,91 +18,134 @@ NetlinkCanParser::~NetlinkCanParser()
 
 }
 
-// NetlinkCanParser::DeviceDetails* NetlinkCanParser::parseDetails(Netlink::Data* data)
-// {
-// 	if( !(data->flags&IFF_UP) )
-// 	{
-// 		fprintf( stdout, "interface is not up\n");
-// 		return 0;
-// 	}
-// 	
-// 	// check if type is can
-// 	
-// 	DeviceDetails* details = new DeviceDetails;
-// 	memset( details, 0, sizeof(DeviceDetails) );
-// 	
-// 	details->ifindex = data->index;
-// 	details->ifname  = data->name;
-// 	details->iftype  = data->type;
-// 	
-// 	if ( data->tb[IFLA_QDISC]     ) 
-// 		details->qdisc = (char*)RTA_DATA(data->tb[IFLA_QDISC]      );
-// 	if ( data->tb[IFLA_OPERSTATE] ) 
-// 		details->state = operationState ( *(uint8_t*)(data->tb[IFLA_OPERSTATE]) );
-// 	
-// 	
-// 	
-// 	return nullptr;
-// }
-//   
-// NetlinkCanParser::DeviceStatistics* NetlinkCanParser::parseStatistics(Netlink::Data* data)
-// {
-// 	if( !(data->flags&IFF_UP) )
-// 	{
-// 		fprintf( stdout, "interface is not up\n");
-// 		return 0;
-// 	}
-// 	
-// 	// check if type is can
-// 	
-// 	DeviceStatistics *stats = new DeviceStatistics;
-// 	memset( stats, 0, sizeof(DeviceStatistics) );
-// 	
-// 	struct rtnl_link_stats64 *s;
-// 	if ( data->tb[IFLA_STATS64] )
-// 	{
-// 		// 64bit statistics
-// 	  
-// 	} else if ( data->tb[IFLA_STATS] )
-// 	{
-// 		// 32bit statistics
-// 	} else
-// 	{
-// 		// no statistics return;
-// 		return stats;
-// 	}
-// 	  
-// 	  
-// 	struct rtnl_link_stats64 slocal;
-// 	s = static_cast<struct rtnl_link_stats64*>(RTA_DATA( data->tb[IFLA_STATS64]));
-// 	
-// 	// Receive stats
-// 	stats->rx_bytes   = s->rx_bytes;
-// 	stats->rx_packets = s->rx_packets;
-// 	stats->rx_errors  = s->rx_errors;
-// 	stats->rx_dropped = s->rx_dropped;
-// 	stats->rx_overrun_errors = s->rx_over_errors;
-// 	stats->rx_multicast      = s->multicast;
-// 	stats->rx_compressed     = s->rx_compressed;
-// 	stats->rx_length_errors  = s->rx_length_errors;
-// 	stats->rx_crc_errors     = s->rx_crc_errors;
-// 	stats->rx_frame_errors   = s->rx_frame_errors;
-// 	stats->rx_fifo_errors    = s->rx_fifo_errors;
-// 	stats->rx_misssed_errors = s->rx_missed_errors;
-// 	
-// 	// Transmit
-// 	
-// 	
-// 	return nullptr;
-// }
-// 
-// NetlinkCanParser::DeviceState NetlinkCanParser::operationState(uint8_t state)
-// {
-// 	if ( state >= static_cast<uint8_t>(DeviceState::STATE_SIZE) )
-// 		return DeviceState::UNKNOWN;
-// 	else
-// 		return static_cast<DeviceState>(state);
-// }
+NetlinkCanParser::CanDeviceDetails* NetlinkCanParser::parseCanDetails(Netlink::Data* data)
+{
+	// check
+	
+	CanDeviceDetails *details = new CanDeviceDetails;
+	memset( details, 0, sizeof(CanDeviceDetails) );
+	
+	if( data->tb[IFLA_CAN_CTRLMODE] )
+		parseControlMode( data, details );
+	if( data->tb[IFLA_CAN_STATE] )
+		details->state = canState( *(unsigned int*)(data->tb[IFLA_CAN_STATE]) );
+	if( data->tb[IFLA_CAN_BERR_COUNTER] )
+		parseBerrCounter( data, details );
+	if( data->tb[IFLA_CAN_BITTIMING] )
+		parseCanBittiming( data, details );
+	if( data->tb[IFLA_CAN_BITTIMING_CONST] )
+		parseCanBittimingConst( data, details);
+	if( data->tb[IFLA_CAN_CLOCK] )
+		parseCanClock( data, details );
+	
+	return details;
+}
+
+std::string NetlinkCanParser::canStateToString(NetlinkCanParser::CanState state)
+{
+	switch( static_cast<unsigned int>(state) )
+	{
+	case static_cast<unsigned int>(CanState::ERROR_ACTIVE):  return "ERROR_ACTIVE";
+	case static_cast<unsigned int>(CanState::ERROR_PASSIV):  return "ERROR_PASSIV";
+	case static_cast<unsigned int>(CanState::ERROR_WARNING): return "ERROR_WARNING";
+	case static_cast<unsigned int>(CanState::BUS_OFF):       return "BUS_OFF";
+	case static_cast<unsigned int>(CanState::SLEEPING):      return "SLEEPING";
+	case static_cast<unsigned int>(CanState::STOPPED):       return "STOPPED";
+	default: return "UNKNOWN";
+	}
+}
+
+std::string NetlinkCanParser::controlModeToString(NetlinkCanParser::ControlMode mode)
+{
+	(void)mode;
+	return "";
+}
+
+
+NetlinkCanParser::CanState NetlinkCanParser::canState(unsigned int data)
+{
+	if( data >= CAN_STATE_MAX )
+	{
+		return CanState::UNKNOWN;
+	}
+	
+	switch( data )
+	{
+	case CAN_STATE_ERROR_ACTIVE: return CanState::ERROR_ACTIVE;
+	case CAN_STATE_ERROR_PASSIVE: return CanState::ERROR_PASSIV;
+	case CAN_STATE_ERROR_WARNING: return CanState::ERROR_WARNING;
+	case CAN_STATE_BUS_OFF:       return CanState::BUS_OFF;
+	case CAN_STATE_SLEEPING:      return CanState::SLEEPING;
+	case CAN_STATE_STOPPED:       return CanState::STOPPED;
+	default: return CanState::UNKNOWN;
+	}
+}
+
+int NetlinkCanParser::parseCanClock(Netlink::Data* data, NetlinkCanParser::CanDeviceDetails* details)
+{
+	struct can_clock *clock = static_cast<can_clock*>(RTA_DATA(data->tb[IFLA_CAN_CLOCK]));
+	details->clock_freq = clock->freq;
+	return 0;
+}
+
+
+int NetlinkCanParser::parseControlMode(Netlink::Data* data, NetlinkCanParser::CanDeviceDetails* details)
+{
+	struct can_ctrlmode *cm = static_cast<struct can_ctrlmode*>(RTA_DATA(data->tb[IFLA_CAN_CTRLMODE]));
+	for( unsigned int i = 0; i < sizeof(cm->flags)*8; i++ )
+	{
+		switch( cm->flags & (1 << i) )
+		{
+		case CAN_CTRLMODE_LOOPBACK:       details->mode.push_back( ControlMode::LOOPBACK );        break;
+		case CAN_CTRLMODE_LISTENONLY:     details->mode.push_back( ControlMode::LISTEN_ONLY );     break;
+		case CAN_CTRLMODE_3_SAMPLES:      details->mode.push_back( ControlMode::TRIPEL_SAMPLING ); break;
+		case CAN_CTRLMODE_ONE_SHOT:       details->mode.push_back( ControlMode::ONE_SHOT );        break;
+		case CAN_CTRLMODE_BERR_REPORTING: details->mode.push_back( ControlMode::BERR_REPORTING );  break;
+		case CAN_CTRLMODE_FD:             details->mode.push_back( ControlMode::FD );              break;
+		case CAN_CTRLMODE_PRESUME_ACK:    details->mode.push_back( ControlMode::PRESUME_ACK );     break;
+		case CAN_CTRLMODE_FD_NON_ISO:     details->mode.push_back( ControlMode::FD_NON_ISO );      break;
+		default: continue;
+		}
+	}
+	return 0;
+}
+
+int NetlinkCanParser::parseBerrCounter(Netlink::Data* data, NetlinkCanParser::CanDeviceDetails* details)
+{
+	struct can_berr_counter *bc = static_cast<struct can_berr_counter*>(RTA_DATA(data->tb[IFLA_CAN_BERR_COUNTER]));
+	details->berr.tx_error = bc->txerr;
+	details->berr.rx_error = bc->rxerr;
+	return 0;
+}
+
+int NetlinkCanParser::parseCanBittiming(Netlink::Data* data, NetlinkCanParser::CanDeviceDetails* details)
+{
+	struct can_bittiming *bt = static_cast<struct can_bittiming*>(RTA_DATA(data->tb[IFLA_CAN_BITTIMING]));
+	details->bittiming.bitrate      = bt->bitrate;
+	details->bittiming.sample_point = bt->sample_point;
+	details->bittiming.tq           = bt->tq;
+	details->bittiming.prop_seg     = bt->prop_seg;
+	details->bittiming.phase_seg1   = bt->phase_seg1;
+	details->bittiming.phase_seg2   = bt->phase_seg2;
+	details->bittiming.sjw          = bt->sjw;
+	details->bittiming.brp          = bt->brp;
+	return 0;
+}
+
+int NetlinkCanParser::parseCanBittimingConst(Netlink::Data* data, NetlinkCanParser::CanDeviceDetails* details)
+{
+	struct can_bittiming_const *btc = static_cast<struct can_bittiming_const*>(RTA_DATA(data->tb[IFLA_CAN_BITTIMING_CONST]));
+	details->const_bittiming.name = btc->name;
+	details->const_bittiming.tseg1_min = btc->tseg1_min;
+	details->const_bittiming.tseg1_max = btc->tseg1_max;
+	details->const_bittiming.tseg2_min = btc->tseg2_min;
+	details->const_bittiming.tseg2_max = btc->tseg2_max;
+	details->const_bittiming.sjw_max   = btc->sjw_max;
+	details->const_bittiming.brp_min   = btc->brp_min;
+	details->const_bittiming.brp_max   = btc->brp_max;
+	details->const_bittiming.brp_inc   = btc->brp_inc;
+	return 0;
+}
 
 
 } /* namespace CanSocket */
--- src/linux/helper/NetlinkCanParser.h
+++ src/linux/helper/NetlinkCanParser.h
@@ -4,21 +4,92 @@
 #include "Netlink.h"
 #include "NetlinkParser.h"
 
-
-
 namespace CanSocket 
 {
 
 class NetlinkCanParser : public NetlinkParser
 {
-
+public:
+	enum struct ControlMode {
+		LOOPBACK,
+		LISTEN_ONLY,
+		TRIPEL_SAMPLING,
+		ONE_SHOT,
+		BERR_REPORTING,
+		FD,
+		PRESUME_ACK,
+		FD_NON_ISO,
+	};
+	
+	enum struct CanState {
+		ERROR_ACTIVE  = 0,
+		ERROR_WARNING = 1,
+		ERROR_PASSIV  = 2,
+		BUS_OFF       = 3,
+		STOPPED       = 4,
+		SLEEPING      = 5,
+		UNKNOWN,
+	};
+	
+	struct BusErrorCounter
+	{
+		uint16_t tx_error;
+		uint16_t rx_error;
+	};
+	
+	struct CanBittiming
+	{
+		uint32_t bitrate;      /* Bit-rate in bits/second */
+		uint32_t sample_point; /* Sample point in one-tenth of a percent */
+		uint32_t tq;           /* Time quanta (TQ) in nanoseconds */
+		uint32_t prop_seg;     /* Propagation segment in TQs */
+		uint32_t phase_seg1;   /* Phase buffer segment 1 in TQs */
+		uint32_t phase_seg2;   /* Phase buffer segment 2 in TQs */
+		uint32_t sjw;          /* Synchronisation jump width in TQs */
+		uint32_t brp;          /* Bit-rate prescaler */
+	};
+	
+	struct CanBittimingConst
+	{
+		std::string name;   /* Name of the CAN controller hardware */
+		uint32_t tseg1_min; /* Time segement 1 = prop_seg + phase_seg1 */
+		uint32_t tseg1_max;
+		uint32_t tseg2_min; /* Time segement 2 = phase_seg2 */
+		uint32_t tseg2_max;
+		uint32_t sjw_max;   /* Synchronisation jump width */
+		uint32_t brp_min;   /* Bit-rate prescaler */
+		uint32_t brp_max;
+		uint32_t brp_inc;
+	};
+	  
+	struct CanDeviceDetails
+	{ 
+		CanState state;
+		uint32_t clock_freq;
+		uint32_t restart_ms;
+		std::vector<ControlMode> mode;
+		BusErrorCounter berr;
+		CanBittiming bittiming;
+		CanBittimingConst const_bittiming;
+	};
   
 public:
 	NetlinkCanParser();
 	~NetlinkCanParser();
 	
-protected:
+	static CanDeviceDetails*    parseCanDetails   (Netlink::Data* data);
 	
+	static std::string canStateToString   (CanState state  );
+	static std::string controlModeToString(ControlMode mode);
+	
+protected:
+	static CanState canState(unsigned int data);
+	
+	static int parseCanClock         (Netlink::Data* data, CanDeviceDetails* details);
+	static int parseControlMode      (Netlink::Data* data, CanDeviceDetails* details);
+	static int parseBerrCounter      (Netlink::Data* data, CanDeviceDetails* details);
+	static int parseCanBittiming     (Netlink::Data* data, CanDeviceDetails* details);
+	static int parseCanBittimingConst(Netlink::Data* data, CanDeviceDetails* details);
 };
 
 } /* namespace CanSocket */
