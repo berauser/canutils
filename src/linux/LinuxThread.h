be026b07fdc931d40d0b716b435292d050a584e4
Benjamin Rauser <benjamin.rauser@gmx.net>
2018-03-04 20:39:55 +0100

    rename Thread implementation

--- src/linux/LinuxThread.h
+++ src/linux/LinuxThread.h
@@ -9,6 +9,8 @@
 #define SRC_MISC_THREAD_H_
 
 #include <thread>
+#include <memory>
+#include <pthread.h>
 
 namespace CanUtils
 {
@@ -28,22 +30,35 @@ public:
 
 	Thread(Thread&& __t) noexcept : std::thread( std::forward<std::thread>(__t) )
 	{
+		pthread_setname_np( this->native_handle(), "SocketCanWorker" );
 	}
 
 	template<typename _Callable, typename... _Args>
 	explicit
 	Thread(_Callable&& __f, _Args&&... __args) : std::thread( __f, (__args)... )
 	{
+		pthread_setname_np( this->native_handle(), "SocketCanWorker" );
 	}
 
-	virtual ~Thread() {}
+	virtual ~Thread()
+	{
+		cancel();
+	}
 
 	/**
 	 * Send cancelling request to thread
 	 * @return On success 0
 	 */
-	int cancel();
+	int cancel()
+	{
+		if( this->joinable() )
+		{
+			return pthread_cancel( this->native_handle() );   
+		}
+		return 0;
+    }
 };
+typedef std::shared_ptr<Thread> ThreadPtr;
 
 } /* namespace CanUtils */
 
